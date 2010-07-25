ae073744be8e1d9015d4db63c5c29a585382f20c
Adrien <onlineallthetime@gmail.com>
2010-07-24 17:59:28 -0700

    anywhere integrated

--- modules.js
+++ modules.js
@@ -1,12 +1,14 @@
 MODULES = [
+
   { 
     sKey: 'welcome',
     title: 'Welcome!',
     css: {width:'300px'},
     initBefore: function() {
-      this.$content.html('This page is <strong><em>pure</em> JavaScript</strong>. Modules can be dragged around, some can be resized. If your browser supports <a href="http://dev.w3.org/html5/webstorage/" target="_blank">Web Storage</a>, module positioning is memorized for your next visit. It works on iPad. <a href="http://github.com/gaarf/modular-page" target="_blank">The source is on github</a>. Enjoy!');
+      this.$content.html('This page is <strong><em>pure</em> JavaScript</strong>. Modules can be dragged around, some can be resized. If your browser supports <a href="http://dev.w3.org/html5/webstorage/">Web Storage</a>, module positioning is memorized for your next visit. It works on iPad. <a href="http://github.com/gaarf/modular-page">The source is on github</a>. Enjoy!');
     }
   },
+
   { 
     sKey: 'lorem',
     title: 'Lorem ipsum',
@@ -20,25 +22,53 @@ MODULES = [
       this.doneLoading();
     }
   },
-  // { 
-  //   sKey: 'twitter',
-  //   _USERNAME: 'gaarf',
-  //   css: {height:'200px',width:'300px',clear:'left'},
-  //   resizable: true,
-  //   initBefore: function() {
-  //     this.getJSONp('http://twitter.com/status/user_timeline/'+this._USERNAME+'.json?count=10&callback=?');
-  //   },
-  //   JSONpCallback: function(data) {
-  //     this.$content.html((data.length+1)+' Tweets received!');
-  //   }
-  // },
-  // { 
-  //   sKey: 'flickr',
-  //   _ID: '94765669@N00',
-  //   _TAGS: 'flickrrss',
-  //   css: {height:'200px',width:'300px', clear:'left'},
-  //   initAfter: function() {
-  //     this.getJSONp('http://api.flickr.com/services/feeds/photos_public.gne?id='+this._ID+'&tags='+this._TAGS+'&format=json&jsoncallback=?');
-  //   }
-  // }
+
+  { 
+    sKey: 'twitter',
+    _USERNAME: 'gaarf',
+    css: {height:'600px',width:'300px',clear:'left'},
+    resizable: true,
+    title: 'Twitter',
+    initBefore: function() {
+      this.getJSONp('http://twitter.com/status/user_timeline/'+this._USERNAME+'.json?count=10&callback=?');
+      this.$node.find('h2').append(' - @'+this._USERNAME);
+    },
+    JSONpCallback: function(data) {
+      var html = '<ol>';
+      $.each(data,function(i){
+        console.log(i,this);
+        var when = new Date(this.created_at);
+        html += '<li class="item'+i+'"><div class="text">';
+        html += this.text.replace( /https?:\/\/[^\s]+/g, function($0) { return '<a href="'+$0+'">'+$0+'</a>'; } );
+        html += '</div> <div class="meta"><span class="when">'+when+'</span>';
+        if(this.place) {
+          var g = this.geo.coordinates;
+          html += ' <span class="geo">From <a href="http://twitter.com/search?q=place%3A'+this.place.id+'">'+this.place.full_name+'</a>';
+          html += ' [<a href="http://maps.google.com/maps?q='+g[0]+','+g[1]+'">map</a>]</span>';
+        }
+        html += '</div></li>';
+      });
+      this.$content.html(html+'</ol>');
+
+      var s = 'li.twitter .follow-me';
+      if(twttr) {
+        var that = this;
+        twttr.anywhere(function(T) {
+          T('li.twitter').hovercards();
+        });
+      }
+
+    }
+  },
+
+  { 
+    sKey: 'flickr',
+    _ID: '94765669@N00',
+    _TAGS: 'flickrrss',
+    css: {height:'200px',width:'300px'},
+    initAfter: function() {
+      this.getJSONp('http://api.flickr.com/services/feeds/photos_public.gne?id='+this._ID+'&tags='+this._TAGS+'&format=json&jsoncallback=?');
+    }
+  }
+
 ];
